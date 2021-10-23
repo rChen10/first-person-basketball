7abcbf527341bd0ca1826e77d38e6c1ad894c355
rchen10 <ricky.chen.2@stonybrook.edu>
2021-10-22 23:02:00 -0400

    Various fixes

--- Source/FPSProject/FPSCharacter.cpp
+++ Source/FPSProject/FPSCharacter.cpp
@@ -119,7 +119,7 @@ void AFPSCharacter::Fire()
 			GetActorEyesViewPoint(CameraLocation, CameraRotation);
 
 			// Set MuzzleOffset to spawn projectiles slightly in front of the camera.
-			MuzzleOffset.Set(100.0f, 0.0f, 0.0f);
+			MuzzleOffset.Set(200.0f, 0.0f, 0.0f);
 
 			// Transform MuzzleOffset from camera space to world space.
 			FVector MuzzleLocation = CameraLocation + FTransform(CameraRotation).TransformVector(MuzzleOffset);
